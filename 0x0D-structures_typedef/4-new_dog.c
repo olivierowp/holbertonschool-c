f0162379b88cba23dd641461b022dca997d0eefc
Olivier <odutartre@onewealthplace.com>
2020-09-01 22:53:28 +0200

    0x0D : free dog

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include "dog.h"
 
 /**
@@ -12,29 +10,21 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
+	char *n, *o;
 	dog_t *new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
 
-	new_dog->name = malloc(_strlen(name) + 1);
-	if (new_dog->name == NULL)
-	{
-		free(new_dog);
+	if (!new_dog || !name || !owner)
 		return (NULL);
-	}
-	new_dog->name = _strdup(name);
-
-	new_dog->owner = malloc(_strlen(owner) + 1);
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (NULL);
-	}
-	new_dog->owner = _strdup(owner);
+	n = malloc(_strlen(name) + 1);
+	if (!n)
+		return (free(new_dog), NULL);
+	n = _strdup(name);
+	new_dog->name = n;
+	o = malloc(_strlen(owner) + 1);
+	if (!o)
+		return (free(new_dog->name), free(new_dog), NULL);
+	o = _strdup(owner);
+	new_dog->owner = o;
 	new_dog->age = age;
 	return (new_dog);
 }
