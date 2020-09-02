408ebb76fba78aff1a01ef0c515a995e702c8312
Olivier <odutartre@onewealthplace.com>
2020-09-02 21:06:59 +0200

    0x0C : preprocessor abuse

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -2,48 +2,62 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- * Return: malloced dog_t OR NULL
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: input char
+ * Return: char
  */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i += 1;
+	i += 1;
+	dup = malloc(i * sizeof(*dup));
+	if (dup == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		dup[j] = str[j];
+		j += 1;
+	}
+	return (dup);
+}
+
+/**
+ *new_dog - function that creates a new dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner:owner of dog
+ *Return: new struct
+ */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int index;
+	struct dog *new_dog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	dog_t *new_dog = malloc(sizeof(dog_t));
-
+	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
-	for (index = 0; name[index]; index++)
-	{
-	}
-	new_dog->name = malloc(sizeof(char) * (index + 1));
+	new_dog->name = _strdup(name);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	for (index = 0; name[index]; index++)
-		new_dog->name[index] = name[index];
-	for (index = 0; owner[index]; index++)
-	{
-	}
-	new_dog->owner = malloc(sizeof(char) * (index + 1));
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog->name);
 		free(new_dog);
+		free(new_dog->name);
 		return (NULL);
 	}
-	for (index = 0; owner[index]; index++)
-		new_dog->owner[index] = owner[index];
-	new_dog->age = age;
 	return (new_dog);
 }
