30c954f8fa8c02b6986b6a86a164474c6b28a588
Olivier <odutartre@onewealthplace.com>
2020-09-02 22:11:06 +0200

    0x80 : Add functions

---
+++ 0x80-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _isupper -checks for an uppercase letter.
+ * @c: letter to be checked
+ * Return: returns 1 OR 0.
+ */
+
+int _isupper(int c)
+{
+	return (0);
+}
---
+++ 0x80-static_libraries/0-memset.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _memset - fill memory with a constant byte
+ * @s: the pointer to the location.
+ * @b:  the char to fill each space.
+ * @n:  The first number of spaces to be fill by b.
+ * Return: a pointer to the memory area.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+}
