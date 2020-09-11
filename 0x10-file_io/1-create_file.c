96d6ebd995970ed945d3f49e69b773289cb694db
Olivier <odutartre@onewealthplace.com>
2020-09-11 19:08:50 +0200

    0x10 : Change creation

--- 0x10-file_io/1-create_file.c
+++ 0x10-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[len])
 			len++;
