50882fbc8d414761b8dabb24ace455ecf7d0309f
Olivier <odutartre@onewealthplace.com>
2020-11-04 16:22:08 +0100

    0x14 : Add server

---
+++ 0x14-sockets/0-server.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+/**
+ * main -  opens an IPv4/TCP socket, and listens to traffic on port 12345
+ * Return: 0 success
+ **/
+int main(void)
+{
+	int sock;
+	int port;
+	struct sockaddr_in addr;
+
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == -1)
+		return (EXIT_FAILURE);
+
+	port = 12345;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port);
+
+if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+		return (EXIT_FAILURE);
+
+	listen(sock, 3);
+	printf("Server listening on port %d\n", port);
+	while (1)
+		;
+	return (EXIT_SUCCESS);
+}
---
+++ 0x14-sockets/README.md
@@ -0,0 +1,15 @@
+# 0x14. Sockets
+
+04/11/2020
+
+0. Listen
+1. Accept 
+2. Client
+3. Roger 
+4. REST API - The Request 
+5. REST API - Queries
+6. REST API - Headers
+7. REST API - Body parameters 
+8. REST API - Create TODO 
+ 9. REST API - Retrieve all TODOs 
+
